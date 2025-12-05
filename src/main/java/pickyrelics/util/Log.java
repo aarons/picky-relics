7bc0ad4206541a74b94c405e7c730c4dbad2fd1c
Aaron Scott <ascott@comfyrainbow.com>
2025-12-05 05:33:47 -1000

    feat: enable conditional info logging via debug flag
    Info messages are now suppressed by default in release builds.
    To enable them, start the game with JVM argument: `-Dpickyrelics.debug=true`

--- src/main/java/pickyrelics/util/Log.java
+++ src/main/java/pickyrelics/util/Log.java
@@ -10,8 +10,21 @@ public class Log {
     private static final String PREFIX = "[Picky Relics] ";
     private static final Logger logger = LogManager.getLogger(Log.class);
 
+    /**
+     * Determines whether info messages are logged.
+     * Defaults to false for release builds, can be enabled by setting system property "pickyrelics.debug" to "true".
+     */
+    private static final boolean DEBUG_MODE = Boolean.parseBoolean(System.getProperty("pickyrelics.debug", "false"));
+
+    /**
+     * Logs an info message if debug mode is enabled.
+     * In release builds, info messages are suppressed by default to avoid console spam.
+     * To enable info logging, start the game with JVM argument: -Dpickyrelics.debug=true
+     */
     public static void info(String message) {
-        logger.info(PREFIX + message);
+        if (DEBUG_MODE) {
+            logger.info(PREFIX + message);
+        }
     }
 
     public static void debug(String message) {
